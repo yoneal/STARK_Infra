9e63faf25fd058ba329f37925b3ff57eb3d05c68
JV Roig <jvroig@gmail.com>
2020-11-23 20:14:29 +0800

    auto msg: 2020-11-23 20:14:29

--- STARK_Parser/js/STARK_settings.js
+++ STARK_Parser/js/STARK_settings.js
@@ -1,11 +1,11 @@
-// const STARK={
-//     'parser_url':'https://pfe5z01r02.execute-api.ap-southeast-1.amazonaws.com/parser',
-//     'deploy_url':'https://pfe5z01r02.execute-api.ap-southeast-1.amazonaws.com/deploy',
-//     'deploy_check_url':'https://pfe5z01r02.execute-api.ap-southeast-1.amazonaws.com/deploy_check'
-// };
-
 const STARK={
-    'parser_url':'http://127.0.0.1:3000/parser',
-    'deploy_url':'http://127.0.0.1:3000/deploy',
-    'deploy_check_url':'http://127.0.0.1:3000/deploy_check'
+     'parser_url':'https://pfe5z01r02.execute-api.ap-southeast-1.amazonaws.com/parser',
+     'deploy_url':'https://pfe5z01r02.execute-api.ap-southeast-1.amazonaws.com/deploy',
+     'deploy_check_url':'https://pfe5z01r02.execute-api.ap-southeast-1.amazonaws.com/deploy_check'
 };
+
+// const STARK={
+//     'parser_url':'http://127.0.0.1:3000/parser',
+//     'deploy_url':'http://127.0.0.1:3000/deploy',
+//     'deploy_check_url':'http://127.0.0.1:3000/deploy_check'
+// };
