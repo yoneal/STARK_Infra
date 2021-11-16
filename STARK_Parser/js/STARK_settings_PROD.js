4eb50ad22549dc928bc1aa0ea659e854cb6168ba
JV Roig <jvroig@gmail.com>
2021-11-16 19:47:27 +0800

    Updated APIG url after nuking old infra

--- STARK_Parser/js/STARK_settings_PROD.js
+++ STARK_Parser/js/STARK_settings_PROD.js
@@ -1,5 +1,5 @@
 const STARK={
-    'parser_url':'https://pfe5z01r02.execute-api.ap-southeast-1.amazonaws.com/parser',
-    'deploy_url':'https://pfe5z01r02.execute-api.ap-southeast-1.amazonaws.com/deploy',
-    'deploy_check_url':'https://pfe5z01r02.execute-api.ap-southeast-1.amazonaws.com/deploy_check'
+    'parser_url':'https://58z1dafmul.execute-api.ap-southeast-1.amazonaws.com/parser',
+    'deploy_url':'https://58z1dafmul.execute-api.ap-southeast-1.amazonaws.com/deploy',
+    'deploy_check_url':'https://58z1dafmul.execute-api.ap-southeast-1.amazonaws.com/deploy_check'
 };
