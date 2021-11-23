3ede98585b497f790583b2066cbdec9470f2adc9
JV Roig <jvroig@gmail.com>
2021-11-23 10:53:02 +0800

    auto msg: 2021-11-23 10:53:02

--- lambda/STARK_CodeGen_Static/main.py
+++ lambda/STARK_CodeGen_Static/main.py
@@ -1,5 +1,5 @@
 #STARK Code Generator component.
-#Produces the customized static content for a STARK system
+#Produces the customized static content for a STARK system.
 
 #Python Standard Library
 import base64
