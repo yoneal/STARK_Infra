1e945189c761d23267cfbd28ec43833ca3e8ea05
JV Roig <jvroig@gmail.com>
2021-11-30 16:54:20 +0800

    auto msg: 2021-11-30 16:54:20

--- STARK_Parser/js/STARK_settings_PROD.js
+++
@@ -1,5 +0,0 @@
-const STARK={
-    'parser_url':'https://58z1dafmul.execute-api.ap-southeast-1.amazonaws.com/parser',
-    'deploy_url':'https://58z1dafmul.execute-api.ap-southeast-1.amazonaws.com/deploy',
-    'deploy_check_url':'https://58z1dafmul.execute-api.ap-southeast-1.amazonaws.com/deploy_check'
-};
