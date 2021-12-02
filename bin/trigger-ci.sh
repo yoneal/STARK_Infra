4ea4853f570dac243a6fe2d2a029f7714d586870
JV Roig <jvroig@gmail.com>
2021-12-02 19:09:15 +0800

    auto msg: 2021-12-02 19:09:15

---
+++ bin/trigger-ci.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Add the bin folder to your PATH to make this file executable so you can conveniently trigger
+# this convenience script instead of add, commit and push individually
+# FIXME: allow actual commit message as an optional parameter 
+
+auto_datetime=$(date '+%Y-%m-%d %H:%M:%S')
+
+git add .
+git commit -m "auto msg: $auto_datetime"
+git push
