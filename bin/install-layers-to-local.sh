c31316c2deaf7bcd5e11b9765e8dff4b872607b8
JV Roig <jvroig@gmail.com>
2021-12-18 17:58:01 +0800

    auto msg: 2021-12-18 17:58:01

--- bin/install-layers-to-local.sh
+++ bin/install-layers-to-local.sh
@@ -2,19 +2,13 @@
 # copies and unzips packaged python layers to your local python path
 
 echo "Hello, these are the detected python system paths:"
-
-# python -c "import sys; for path in sys.path: if 'site-packages' in path: print(path)"
 echo -e "import sys\nfor path in sys.path:\n  if 'site-packages' in path: print(path)" | python
 
 echo "Where do you want to install your lambda layers for local access? (copy-paste the full path here):"
-
 read installpath
-
 cp -rf ../lambda/helpers/* $installpath
 
 echo "Installed custom layers in "$installpath"!"
-
 echo "Will now install third-party packages..."
-
 pip install boto3 pyyaml
 
