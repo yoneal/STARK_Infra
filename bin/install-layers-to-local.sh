7801ea2dbe5ea1bcfc5503e0787094cfa34d4899
JV Roig <jvroig@192.168.254.101>
2021-12-31 11:20:14 +0800

    auto msg: 2021-12-31 11:20:14

--- bin/install-layers-to-local.sh
+++ bin/install-layers-to-local.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 # copies and unzips packaged python layers to your local python path
+# Usage: Argument ($1) is optional. It will default to "python" as the pythong command,
+#     which is the correct Python 3.x command for most modern systems.
+#     If "python" in your setup still means Python 2.7, then you can supply the correct
+#     Python 3.x command by passing it as the argument. 
+#     For example, if the correct command is "python3", then trigger this script using:
+#           ./install-layers-to-local.sh python3
+
+if [[ $1 == '' ]]
+then
+    pycommand="python"
+else
+    pycommand=$1
+fi
 
 echo "Hello, these are the detected python system paths:"
-echo -e "import sys\nfor path in sys.path:\n  if 'site-packages' in path: print(path)" | python
+echo -e "import sys\nfor path in sys.path:\n  if 'site-packages' in path: print(path)" | $1
 
 echo "Where do you want to install your lambda layers for local access? (copy-paste the full path here):"
 read installpath
