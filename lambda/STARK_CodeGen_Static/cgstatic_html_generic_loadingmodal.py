3c358a86ef6f0979b7bdaa243da0bc39082a1853
JV Roig <jvroig@gmail.com>
2022-01-06 12:10:56 +0800

    auto msg: 2022-01-06 12:10:56

--- lambda/STARK_CodeGen_Static/cgstatic_html_generic_loadingmodal.py
+++ lambda/STARK_CodeGen_Static/cgstatic_html_generic_loadingmodal.py
@@ -4,23 +4,24 @@
 def create():
 
     source_code = f"""\
-        <div>
-            <b-modal id="loading-modal"
-                no-close-on-backdrop
-                no-close-on-esc
-                hide-header
-                hide-footer
-                centered
-                size="sm">
-                <div class="text-center p-3">
-                    <div class="spinner-border" style="width: 5rem; height: 5rem" role="status">
-                        <span class="sr-only">Loading...</span>
+            <div>
+                <b-modal id="loading-modal"
+                    no-close-on-backdrop
+                    no-close-on-esc
+                    hide-header
+                    hide-footer
+                    centered
+                    size="sm">
+                    <div class="text-center p-3">
+                        <div class="spinner-border" style="width: 5rem; height: 5rem" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                        <div class="mt-3">
+                            Loading...
+                        </div>
                     </div>
-                    <div class="mt-3">
-                        Loading...
-                    </div>
-                </div>
-            </b-modal>
-        </div>"""
+                </b-modal>
+            </div>
+"""
 
     return source_code
