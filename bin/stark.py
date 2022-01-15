d9162ff96ca6393de70f9c8fc561fde7905ce42b
JV Roig <jvroig@gmail.com>
2022-01-15 17:05:25 +0800

    auto msg: 2022-01-15 17:05:25

--- bin/stark.py
+++ bin/stark.py
@@ -52,3 +52,20 @@ construct_type = construct[0]
 construct_file = construct[1]
 
 print(f"Will now create new {construct_type} construct, using {construct_file}...")
+
+#New Module Construct: Sequence
+#   1. STARK Parser - parse supplied YAML file (entity information)
+
+#   2. Get entity information only (no need for API G creation, S3 creation, DDB creation; these are for New Project)
+#   2.1 Specifically: parse_dynamodb (data model in cloud_resources['DynamoDB']['Models'], a.k.a. DDB Model) components
+#   2.2 That basically encapsulates everything we need, and is the only thing CGDynamic and CGStatic really need (aside from S3 buckets & Git repos)
+#   2.3 Note that while CGDynamic gets the entity list from CodeGen Metadata, it could also have just easily derived that from the DDB Models, like how CGStatic does it.
+
+#   3. Create CGStatic output based on DDB Models from #2.1 (static HTML, CSS, and JS inside the `static` folder)
+
+#   4. Create CGDynamic output based on DD Models from #2.1 (new lambda packages inside the `lambda` folder)
+
+#   5. Add the generated cloud_resources DDB Model as new entries in the project's existing cloud_resources
+#   5.1 Since that's a YAML file, read exsting file, load as python Dict. 
+#   5.2 Add new entries to the cloud_resources Dict under DDB Models. 
+#   5.3 Export back to YAML and write new cloud_resources (similar to how STARK_Parser does it)
