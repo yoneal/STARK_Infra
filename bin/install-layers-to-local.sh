a88c035300af4a39a5d23174fd11227f657bb8e1
JV Roig <jvroig@gmail.com>
2022-01-17 17:36:31 +0800

    auto msg: 2022-01-17 17:36:31

--- bin/install-layers-to-local.sh
+++ bin/install-layers-to-local.sh
@@ -14,7 +14,11 @@ else
     pycommand=$1
 fi
 
-echo "Hello, these are the detected python system paths:"
+echo "Will install third-party packages..."
+pip install boto3 pyyaml crhelper bcrypt
+
+echo "Will now proceed to install lambda layers for local access."
+echo "These are the detected python system paths:"
 echo -e "import sys\nfor path in sys.path:\n  if 'site-packages' in path: print(path)" | $pycommand
 
 echo "Where do you want to install your lambda layers for local access? (copy-paste the full path here):"
@@ -22,5 +26,3 @@ read installpath
 cp -rf ../lambda/helpers/* $installpath
 
 echo "Installed custom layers in "$installpath"!"
-echo "Will now install third-party packages..."
-pip install boto3 pyyaml crhelper bcrypt
