f836c631438df53dba0081fa7d1916937d944a77
JV Roig <jvroig@gmail.com>
2022-01-18 14:22:00 +0800

    auto msg: 2022-01-18 14:22:00

--- lambda/STARK_Parser/parse_dynamodb.py
+++ lambda/STARK_Parser/parse_dynamodb.py
@@ -55,7 +55,7 @@ def parse(data):
     for entity in entities:
         parsed["Models"][entity] = {}
         parsed["Models"][entity]["pk"] = data_model.get(entity).get('pk')
-        parsed["Models"][entity]["data"] = OrderedDict() 
+        parsed["Models"][entity]["data"] = {}
 
         attributes = ''
         for column_dict in data_model.get(entity).get("data"):
