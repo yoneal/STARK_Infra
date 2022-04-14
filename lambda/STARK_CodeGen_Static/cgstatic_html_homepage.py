810ff1db46e66d4b22110f588bdbf8e7dc94506f
JV Roig <jvroig@gmail.com>
2022-04-14 21:43:27 +0800

    auto msg: 2022-04-14 21:43:27

--- lambda/STARK_CodeGen_Static/cgstatic_html_homepage.py
+++ lambda/STARK_CodeGen_Static/cgstatic_html_homepage.py
@@ -16,23 +16,29 @@ def create(data):
     source_code += cg_bodyhead.create(data, "_HomePage")
 
     source_code += f"""\
-            <div class="d-flex align-content-start flex-wrap">
-                <template v-for="module in modules" id="modules-template">
-                    <!--
-                    <div class="p-0 m-2 border border-primary module_card w-100 text-nowrap" style="overflow-x: hidden" id="modules_list" :onclick="'window.location.href=\\''  + module.href + '\\''">
-                        <img class="bg-primary p-2 m-0" :src="module.image" :alt="module.image_alt" height="50px">
-                        <span class="card-text align-middle font-weight-bold pl-2 pr-4">{{{{ module.title }}}}</span>                
+            <template v-for="(group, index) in modules" id="groups-template">
+                <!-- <b-button squared size="lg" v-b-toggle="'group-collapse-'+index" variant="light" class="mb-0 pl-2"> -->        
+                <h4>
+                    <a v-b-toggle class="text-decoration text-dark" :href="'#group-collapse-'+index" @click.prevent>
+                        <span class="align-bottom">{{ group.group_name }}</span>
+                        <span class="when-open"><img src="images/chevron-up.svg" height="20rem"></span><span class="when-closed"><img src="images/chevron-down.svg" height="20rem"></span>
+                    </a>
+                </h4>
+                <!-- </b-button> -->
+                <b-collapse :id="'group-collapse-'+index" visible class="mt-0 mb-2 pl-2">
+                    <div class="d-flex align-content-start flex-wrap">
+                        <template v-for="module in group.modules" id="modules-template">
+                            <div class="card p-0 m-2 border module_card" id="modules_list" :onclick="'window.location.href=\''  + module.href + '\''">
+                                <img class="card-img-top p-3 m-0" :src="module.image" alt="Card image cap" height="100rem">
+                                <div class="card-body bg-primary p-2 rounded-bottom">
+                                    <p class="card-text text-light font-weight-bold text-center">{{module.title}}</p>
+                                </div>
+                            </div>
+                        </template>
                     </div>
-                    -->
-                    <div class="card p-0 m-2 border module_card" id="modules_list" :onclick="'window.location.href=\\''  + module.href + '\\''">
-                        <img class="card-img-top p-3  m-0" :src="module.image" alt="Card image cap" height="100px">
-                        <div class="card-body bg-primary p-2 rounded-bottom">
-                            <p class="card-text text-light font-weight-bold text-center">{{{{module.title}}}}</p>
-                        </div>
-                    </div>
-
-                </template>
-            </div>
+                </b-collapse>
+                <hr><br>
+            </template>
 
         </div>
 """
--- lambda/STARK_CodeGen_Static/cgstatic_js_homepage.py
+++ lambda/STARK_CodeGen_Static/cgstatic_js_homepage.py
@@ -10,14 +10,43 @@ def create(data):
         var root = new Vue({{
             el: "#vue-root",
             data: {{
+                group_collapse_name: 'group-collapse-1',
                 modules: ''
             }},
             methods:{{
                 get_module_list: function () {{
-                    fetchUrl = STARK.sys_modules_url
+                    fetchUrl = STARK.STARK_Module_url + '?rt=usermodules'
                     STARK.request('GET', fetchUrl)
                     .then( function(data) {{
-                        root.modules = data;
+
+                        console.log(typeof(data))
+                        const grouped_modules = []
+
+                        grouping = []
+                        grouping_ctr = 0
+                        for (const key in data) {{
+                            console.log(data[key]['group'])
+                            group = data[key]['group']
+                            if ( group in grouping ) {{
+                                //Skip
+                            }}
+                            else {{
+                                grouping[group] = grouping_ctr
+                                grouped_modules[grouping_ctr] = {{
+                                    "group_name": group,
+                                    "modules": []
+                                }}
+                                grouping_ctr++
+                            }}
+
+                            grouped_modules[grouping[group]]["modules"].push(data[key])
+                        }}
+
+                        console.log(grouped_modules)
+                        console.log(data)
+                        console.log(typeof(data))
+                        console.log(typeof(grouped_modules))
+                        root.modules = grouped_modules;
                         console.log("DONE! Retrieved list of modules.")
                         spinner.hide();
                     }})
@@ -29,9 +58,5 @@ def create(data):
         }})
 
         root.get_module_list();
-
-
-
-
     """
     return textwrap.dedent(source_code)
