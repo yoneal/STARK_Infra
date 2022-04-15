e09ff4b8aede4a95c106abad8c9e6a3f2a3612d2
JV Roig <jvroig@gmail.com>
2022-04-15 17:12:21 +0800

    auto msg: 2022-04-15 17:12:21

--- lambda/STARK_CodeGen_Static/cgstatic_html_homepage.py
+++ lambda/STARK_CodeGen_Static/cgstatic_html_homepage.py
@@ -20,7 +20,7 @@ def create(data):
                 <!-- <b-button squared size="lg" v-b-toggle="'group-collapse-'+index" variant="light" class="mb-0 pl-2"> -->        
                 <h4>
                     <a v-b-toggle class="text-decoration text-dark" :href="'#group-collapse-'+index" @click.prevent>
-                        <span class="align-bottom">{{ group.group_name }}</span>
+                        <span class="align-bottom">{{{{ group.group_name }}}}</span>
                         <span class="when-open"><img src="images/chevron-up.svg" height="20rem"></span><span class="when-closed"><img src="images/chevron-down.svg" height="20rem"></span>
                     </a>
                 </h4>
@@ -28,10 +28,10 @@ def create(data):
                 <b-collapse :id="'group-collapse-'+index" visible class="mt-0 mb-2 pl-2">
                     <div class="d-flex align-content-start flex-wrap">
                         <template v-for="module in group.modules" id="modules-template">
-                            <div class="card p-0 m-2 border module_card" id="modules_list" :onclick="'window.location.href=\''  + module.href + '\''">
+                            <div class="card p-0 m-2 border module_card" id="modules_list" :onclick="'window.location.href=\\''  + module.href + '\\''">
                                 <img class="card-img-top p-3 m-0" :src="module.image" alt="Card image cap" height="100rem">
                                 <div class="card-body bg-primary p-2 rounded-bottom">
-                                    <p class="card-text text-light font-weight-bold text-center">{{module.title}}</p>
+                                    <p class="card-text text-light font-weight-bold text-center">{{{{module.title}}}}</p>
                                 </div>
                             </div>
                         </template>
