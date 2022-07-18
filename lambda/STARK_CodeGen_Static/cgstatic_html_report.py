0dfabf5abfd6d27ba4f535d247f75e44c0663173
Mark Danico A. Fernandez <nico@serverless.ninja>
2022-07-18 17:24:00 +0800

    Change button class for back in generated report

--- lambda/STARK_CodeGen_Static/cgstatic_html_report.py
+++ lambda/STARK_CodeGen_Static/cgstatic_html_report.py
@@ -102,7 +102,7 @@ def create(data):
             <div class="container" v-if="showReport">
                 <div class="row">
                     <div class="col-6 text-left d-inline-block">
-                        <button id="prev" type="button" class="btn btn-outline-secondary mb-2" onClick="root.showReport = false"> Back </button>
+                        <button id="prev" type="button" class="btn btn-secondary mb-2" onClick="root.showReport = false"> Back </button>
                         <button type="button" class="btn btn-primary mb-2" onClick="root.download_csv()"> Export as CSV</button>
                         <button id="refresh" type="button" class="btn btn-success mb-2"> Refresh </button>
                     </div>
