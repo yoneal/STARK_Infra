a3ab1db0bfa6bb407ca63a4c67369cdf1f1c32f4
JV Roig <jvroig@gmail.com>
2022-07-25 22:17:47 +0800

    Modify builder behavior to avoid nested dependencies

--- lambda/STARK_CodeGen_Dynamic/cgdynamic_buildspec.py
+++ lambda/STARK_CodeGen_Dynamic/cgdynamic_buildspec.py
@@ -24,6 +24,7 @@ def create(data):
                 - BUCKET=$(cat template_configuration.json | python3 -c "import sys, json; print(json.load(sys.stdin)['Parameters']['UserCICDPipelineBucketNameParameter'])")
                 - WEBSITE=$(cat template_configuration.json | python3 -c "import sys, json; print(json.load(sys.stdin)['Parameters']['UserWebsiteBucketNameParameter'])")
                 - sed -i "s/RandomTokenFromBuildScript/$(date)/" template.yml
+                - cp -R lambda lambda_src
                 - pip install pyyaml
                 - python3 ./builder.py
                 - aws cloudformation package --template-file template.yml --s3-bucket $BUCKET --s3-prefix {project_varname} --output-template-file outputtemplate.yml
