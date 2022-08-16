d7e0d5609ab44adf67661b422733104921f7367c
Kim Reyes <reyeskimberly018@gmail.com>
2022-08-16 09:13:25 +0800

    Update view and delete pill in BM v3

--- lambda/STARK_CodeGen_Static/cgstatic_html_delete.py
+++ lambda/STARK_CodeGen_Static/cgstatic_html_delete.py
@@ -43,12 +43,14 @@ def create(data):
                             </div>"""
     # print(cols)
     for col, col_type in cols.items():
-        entities = cg_rel.get({
-            "col": col,
-            "col_type": col_type,
-        })
+        print(col)
+        print(col_type)
+    #     entities = cg_rel.get({
+    #         "col": col,
+    #         "col_type": col_type,
+    #     })
     
-    print(entities)
+    # print(entities)
         
 #         for related in entities:
 #             print(related)
