af0e728093ab8d4520d5dab9f0c2a90d61ca76a4
Kim Reyes <reyeskimberly018@gmail.com>
2022-08-16 10:22:53 +0800

    Update view and delete pill in BM v8

--- lambda/STARK_CodeGen_Static/cgstatic_html_delete.py
+++ lambda/STARK_CodeGen_Static/cgstatic_html_delete.py
@@ -44,7 +44,7 @@ def create(data):
     # print(cols)
     for col, col_type in cols.items():
         # print(col)
-        print(col_type["type"])
+        # print(col_type["type"])
         entities = cg_rel.get({
             "col": col,
             "col_type": col_type,
@@ -52,37 +52,37 @@ def create(data):
     
         # print(entities)
         
-        # for related in entities:
-            # print(related)
-       
-        #         foreign_entity  = converter.convert_to_system_name(has_many)
+        for related in entities:
+            print(related)
+            foreign_entity  = converter.convert_to_system_name(related)
+            source_code += f"""
+            <div class="form-group row">
+                <label for="{foreign_entity}" class="col-sm-2 col-form-label">{foreign_entity}</label>
+                <div class="col-sm-10">
+                    <b-form-group label-for="tags-with-dropdown">
+                        <b-form-tags id="tags-with-dropdown" v-model="multi_select_values.{foreign_entity}" no-outer-focus class="mb-2">
+                            <template v-slot="{{ tags, disabled, addTag, removeTag }}">
+                                <ul v-if="tags.length > 0" class="list-inline d-inline-block mb-2">
+                                    <li v-for="tag in tags" :key="tag" class="list-inline-item">
+                                        <b-form-tag 
+                                            @remove="removeTag(tag)" 
+                                            :title="tag" 
+                                            :disabled="true" 
+                                            variant="info" >
+                                            {{ tag_display_text(tag) }}
+                                        </b-form-tag>
+                                    </li>
+                                </ul>
+                            </template>
+                        </b-form-tags>
+                    </b-form-group>
+                </div>
+            </div>
+            """      
         #         print(foreign_entity)
             # foreign_entity  = converter.convert_to_system_name(has_many)
             # if has_many != '':
-            #     source_code += f"""
-            #     <div class="form-group row">
-            #         <label for="{foreign_entity}" class="col-sm-2 col-form-label">{foreign_entity}</label>
-            #         <div class="col-sm-10">
-            #             <b-form-group label-for="tags-with-dropdown">
-            #                 <b-form-tags id="tags-with-dropdown" v-model="multi_select_values.{foreign_entity}" no-outer-focus class="mb-2">
-            #                     <template v-slot="{{ tags, disabled, addTag, removeTag }}">
-            #                         <ul v-if="tags.length > 0" class="list-inline d-inline-block mb-2">
-            #                             <li v-for="tag in tags" :key="tag" class="list-inline-item">
-            #                                 <b-form-tag 
-            #                                     @remove="removeTag(tag)" 
-            #                                     :title="tag" 
-            #                                     :disabled="true" 
-            #                                     variant="info" >
-            #                                     {{ tag_display_text(tag) }}
-            #                                 </b-form-tag>
-            #                             </li>
-            #                         </ul>
-            #                     </template>
-            #                 </b-form-tags>
-            #             </b-form-group>
-            #         </div>
-            #     </div>
-            #     """
+            
 
 
 
