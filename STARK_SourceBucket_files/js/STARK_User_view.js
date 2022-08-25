76c200e34df9533e669c8e195133af3e840d0896
Kim Reyes <reyeskimberly018@gmail.com>
2022-08-25 17:22:59 +0800

    Implement STARK auth for all pages v6

--- STARK_SourceBucket_files/js/STARK_User_view.js
+++ STARK_SourceBucket_files/js/STARK_User_view.js
@@ -197,18 +197,7 @@ var root = new Vue({
 
        list: function (lv_token='', btn='') {
             spinner.show()
-            data = {}
-            data['stark_permissions'] = this.stark_permissions
-            STARK.auth(data).then( function(data) {
-                console.log("Auth Request Done!");
-                console.log(data);
-                root.stark_permissions = data;
-            })
-            .catch(function(error) {
-                console.log("Encountered an error! [" + error + "]")
-                alert("Request Failed: System error or you may not have enough privileges")
-                loading_modal.hide()
-            });
+           
             payload = []
             if (btn == 'next') {
                 root.curr_page++;
