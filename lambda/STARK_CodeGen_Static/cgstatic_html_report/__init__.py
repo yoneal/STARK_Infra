a72e22bcc7dc4a36ad1b97f25362f67af2e2d3fc
Kim Reyes <reyeskimberly018@gmail.com>
2022-08-25 18:34:35 +0800

    Implement STARK auth for all pages v7

--- lambda/STARK_CodeGen_Static/cgstatic_html_report/__init__.py
+++ lambda/STARK_CodeGen_Static/cgstatic_html_report/__init__.py
@@ -154,8 +154,8 @@ def create(data):
                                     <template v-for="{entity_varname} in listview_table" id="listview-table">
                                         <tr>
                                             <td align="center">
-                                                <a :href="'{entity_varname}_edit.html?{pk_varname}=' + {entity_varname}.{pk_varname}" target="_blank" v-if="stark_permissions['{entity}|Edit']"><img src="images/pencil-square.svg" class="bg-info"></a>
-                                                <a :href="'{entity_varname}_delete.html?{pk_varname}=' + {entity_varname}.{pk_varname}" target="_blank" v-if="stark_permissions['{entity}|Delete']"><img src="images/x-square.svg" class="bg-danger"></a>
+                                                <a :href="'{entity_varname}_edit.html?{pk_varname}=' + {entity_varname}.{pk_varname}" target="_blank" v-if="auth_list.Edit.allowed"><img src="images/pencil-square.svg" class="bg-info"></a>
+                                                <a :href="'{entity_varname}_delete.html?{pk_varname}=' + {entity_varname}.{pk_varname}" target="_blank" v-if="auth_list.Delete.allowed"><img src="images/x-square.svg" class="bg-danger"></a>
                                             </td>
                                             <template v-for="column in STARK_report_fields">
                                                 <td>{{{{ {entity_varname}[column.field] }}}}</td>
