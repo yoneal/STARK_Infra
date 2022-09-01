0dbff70279b7c832c8568a1dc392feab13925f78
Kim Reyes <reyeskimberly018@gmail.com>
2022-09-01 09:57:47 +0800

    Update html indentation and spaces

--- lambda/STARK_CodeGen_Static/cgstatic_html_homepage/__init__.py
+++ lambda/STARK_CodeGen_Static/cgstatic_html_homepage/__init__.py
@@ -23,30 +23,31 @@ def create(data):
     source_code += cg_bodyhead.create(data, "_HomePage")
 
     source_code += f"""\
-            <template v-for="(group, index) in modules" id="groups-template">
-                <!-- <b-button squared size="lg" v-b-toggle="'group-collapse-'+index" variant="light" class="mb-0 pl-2"> -->        
-                <h4>
-                    <a v-b-toggle class="text-decoration-none" :href="'#group-collapse-'+index" @click.prevent>
-                        <span class="when-open"><img src="images/chevron-up.svg" class="filter-fill-svg-link" height="20rem"></span><span class="when-closed"><img src="images/chevron-down.svg" class="filter-fill-svg-link" height="20rem"></span>
-                        <span class="align-bottom">{{{{ group.group_name }}}}</span>
-                    </a>
-                </h4>
-                <!-- </b-button> -->
-                <b-collapse :id="'group-collapse-'+index" visible class="mt-0 mb-2 pl-2">
-                    <div class="d-flex align-content-start flex-wrap">
-                        <template v-for="module in group.modules" id="modules-template">
-                            <div class="card p-0 m-2 border module_card" id="modules_list" :onclick="'window.location.href=\\''  + module.href + '\\''">
-                                <img class="card-img-top p-3 m-0 filter-fill-svg" :src="module.image" alt="Card image cap" height="100rem">
-                                <div class="card-body bg-info p-2 rounded-bottom">
-                                    <p class="card-text text-light font-weight-bold text-center">{{{{module.title}}}}</p>
-                                </div>
+        <template v-for="(group, index) in modules" id="groups-template">
+            <!-- <b-button squared size="lg" v-b-toggle="'group-collapse-'+index" variant="light" class="mb-0 pl-2"> -->        
+            <h4>
+                <a v-b-toggle class="text-decoration-none" :href="'#group-collapse-'+index" @click.prevent>
+                    <span class="when-open"><img src="images/chevron-up.svg" class="filter-fill-svg-link" height="20rem"></span><span class="when-closed"><img src="images/chevron-down.svg" class="filter-fill-svg-link" height="20rem"></span>
+                    <span class="align-bottom">{{{{ group.group_name }}}}</span>
+                </a>
+            </h4>
+            <!-- </b-button> -->
+            <b-collapse :id="'group-collapse-'+index" visible class="mt-0 mb-2 pl-2">
+                <div class="d-flex align-content-start flex-wrap">
+                    <template v-for="module in group.modules" id="modules-template">
+                        <div class="card p-0 m-2 border module_card" id="modules_list" :onclick="'window.location.href=\\''  + module.href + '\\''">
+                            <img class="card-img-top p-3 m-0 filter-fill-svg" :src="module.image" alt="Card image cap" height="100rem">
+                            <div class="card-body bg-info p-2 rounded-bottom">
+                                <p class="card-text text-light font-weight-bold text-center">{{{{module.title}}}}</p>
                             </div>
-                        </template>
-                    </div>
-                </b-collapse>
-                <hr><br>
-            </template>
+                        </div>
+                    </template>
+                </div>
+            </b-collapse>
+            <hr><br>
+        </template>
 
+    </div>
     <!-- </div>-->
 """
     source_code += cg_loadspin.create()
