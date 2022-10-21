b1b57b9deec03f32891a38eeb1da87e0c230c511
Kim Reyes <reyeskimberly018@gmail.com>
2022-10-21 14:54:52 +0800

    1-M: Update lamda with _many functions v17

--- lambda/STARK_CodeGen_Static/cgstatic_html_add/__init__.py
+++ lambda/STARK_CodeGen_Static/cgstatic_html_add/__init__.py
@@ -67,7 +67,7 @@ def create(data):
 
         if isinstance(col_type, dict) and col_type["type"] == "relationship":
             has_many_ux = col_type.get('has_many_ux', None)
-            child_entity = col_type.get('has_many')
+            child_entity = col_type.get('has_many', '')
             child_entity_varname = converter.convert_to_system_name(child_entity)
             if has_many_ux: 
                 source_code += f"""
