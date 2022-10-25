e78e98b4460ebdbd043aff55defc7a408a535553
Kim Reyes <reyeskimberly018@gmail.com>
2022-10-25 11:28:06 +0800

    1-M: Update html controls v28

--- lambda/STARK_CodeGen_Static/cgstatic_html_generic_header/__init__.py
+++ lambda/STARK_CodeGen_Static/cgstatic_html_generic_header/__init__.py
@@ -61,21 +61,23 @@ def create(data, special="none"):
             <script src="js/{entity_varname}_app.js" defer></script>
             <script src="js/{entity_varname}_view.js" defer></script>"""
 
-        for rel, rel_col_data in rel_model:
+        print('rel_model')
+        print(rel_model)
+        for rel in rel_model:
             print('rel')
             print(rel)
-            print('rel_col_data')
-            print(rel_col_data)
+            # print('rel_col_data')
+            # print(rel_col_data)
             many_entity_varname = converter.convert_to_system_name(rel)
             source_code += f"""
             <script src="js/many_{many_entity_varname}.js"></script>"""
-            for rel_col, rel_col_type in rel_col_data.items():
-                if isinstance(rel_col_type, dict) and rel_col_type["type"] == "relationship":
-                    has_one = rel_col_type.get('has_one', '')
-                    if  has_one != '':
-                        foreign_entity  = converter.convert_to_system_name(has_one)
-                        source_code += f"""
-            <script src="js/{foreign_entity}_app.js"></script>"""
+            # for rel_col, rel_col_type in rel_col_data.items():
+            #     if isinstance(rel_col_type, dict) and rel_col_type["type"] == "relationship":
+            #         has_one = rel_col_type.get('has_one', '')
+            #         if  has_one != '':
+            #             foreign_entity  = converter.convert_to_system_name(has_one)
+            #             source_code += f"""
+            # <script src="js/{foreign_entity}_app.js"></script>"""
 
         #Figure out which other _app.js files we need to add based on relationships
         for col, col_type in cols.items():
