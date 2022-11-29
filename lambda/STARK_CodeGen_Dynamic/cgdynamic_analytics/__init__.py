47a8d2c67045d74272e128bb96e59e9a2373c000
Mark Danico A. Fernandez <nico@serverless.ninja>
2022-11-29 13:40:41 +0800

    Fix bug in  deleting processed bucket on first run

--- lambda/STARK_CodeGen_Dynamic/cgdynamic_analytics/__init__.py
+++ lambda/STARK_CodeGen_Dynamic/cgdynamic_analytics/__init__.py
@@ -26,7 +26,7 @@ def create(data):
         #FIXME: Temporary solution to duplication of records due to multiple parquet files read in processed bucket
         #       Delete every files inside the processed bucket        
         client = boto3.client('s3')
-        resp = client.list_objects(Bucket=stark_core.analytics_processed_bucket_name)['Contents']
+        resp = client.list_objects(Bucket=stark_core.analytics_processed_bucket_name).get('Contents',{{}})
         for val in resp:
             client.delete_object(Bucket=stark_core.analytics_processed_bucket_name, Key=val["Key"])
 
