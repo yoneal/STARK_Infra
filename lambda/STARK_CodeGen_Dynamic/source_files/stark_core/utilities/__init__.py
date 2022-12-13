51e20b83ed31df8f4c204825b45ebfa756a5b37b
Kim Reyes <reyeskimberly018@gmail.com>
2022-12-13 14:36:22 +0800

    Fix issue when filtering pk in Reporting v2

--- lambda/STARK_CodeGen_Dynamic/source_files/stark_core/utilities/__init__.py
+++ lambda/STARK_CodeGen_Dynamic/source_files/stark_core/utilities/__init__.py
@@ -38,7 +38,7 @@ def compose_report_operators_and_parameters(key, data, metadata):
         composed_filter_dict['expression_values'][f":to{key}"] = {data['type'] : from_to_split[1].strip()}
         composed_filter_dict['report_params'] = {key : f"Between {from_to_split[0].strip()} and {from_to_split[1].strip()}"}
     else:
-        if key is not 'pk':
+        if key != 'pk':
             if metadata[key]['data_type'] == 'list' and data['operator'] == '=':
                 composed_filter_dict['filter_string'] += f" contains({key}, :{key}) AND"
             else:
